1efc8496f70f793847b030cb4867dd431c94616e
Jamal Mahmoud <bluedocjamal@hotmail.co.uk>
2017-05-05 17:05:27 +0100

    Added a plane

---
+++ LabTest/Assets/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour {
+
+    public GameObject atom;
+
+    public int NumberOfAtoms = 3;
+    public float distance = 200.0f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
